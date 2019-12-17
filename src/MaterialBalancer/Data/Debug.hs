894eeb80d4ea0c085ef87b47ed9721fb990a4fa8
YongJoon Joe <developer@quietjoon.net>
2019-12-17 13:31:09 +0900

    Showing better structure with showValueStorageWithIdx

--- src/MaterialBalancer/Data/Debug.hs
+++ src/MaterialBalancer/Data/Debug.hs
@@ -16,44 +16,106 @@ import           Data.Ord                       ( comparing )
 
 
 -- NOTE: For testing, use code:
--- mapM_ putStrLn . showValueStorageWithIdx (IM . I.fromList $ Prelude.zip [0..20] $ Prelude.map ( Just . T.pack . show ) ['a','b'..])
--- mapM_ putStrLn . showValueStorageWithIdx . RM . I.fromList $ [(0,(IM . I.fromList $ Prelude.zip [0..20] $ Prelude.map ( Just . T.pack . show ) ['a','b'..]))]
+-- putStrLn . showValueStorageWithIdx (IM . I.fromList $ Prelude.zip [0..20] $ Prelude.map ( Just . T.pack . show ) ['a','b'..])
+-- putStrLn . showValueStorageWithIdx . RM . I.fromList $ [(0,(IM . I.fromList $ Prelude.zip [0..12] $ Prelude.map ( Just . T.pack . show ) ['a','b'..])),(1,(IM . I.fromList $ Prelude.zip [0..12] $ Prelude.map ( Just . T.pack . show ) ['a','b'..]))]
+
+{- α β γ δ
+α--+--+-0=0:1=0:2=0: Just
+β  +  +-0=0:1=0:2=1: Nothing
+β  +  --0=0:1=0:2=2: Nothing
+α  +--+-0=0:1=1:2=0: Just
+β  +  +-0=0:1=1:2=1: Just
+β  +  --0=0:1=1:2=2: Nothing
+γ  ---+-0=0:1=3:2=0: Just
+δ     --0=0:1=3:2=1: Nothing
+-}
 
 showValueStorageWithIdx :: ValueStorage -> String
-showValueStorageWithIdx vs = unlines this
- where
-  this = showValueStorageWithIdxSub vs 0
+showValueStorageWithIdx vs = unlines . showValueStorageWithIdxSub vs [] $ 0
 
-showValueStorageWithIdxSub :: ValueStorage -> Int -> [String]
-showValueStorageWithIdxSub (RM rm) depth = labeledElements preprocessed
+showValueStorageWithIdxSub :: ValueStorage -> Keys -> Int -> [String]
+showValueStorageWithIdxSub (RM rm) accKeys depth = headerElements preprocessed
  where
-  len = getDecimalLen . I.size $ rm
-  preprocessed :: [(Int,[String])]
-  preprocessed = map (\(k,v) -> (k, (showValueStorageWithIdxSub v (depth+1)))) . I.toList $ rm
-  labeledElements :: [(Int,[String])] -> [String]
-  labeledElements [] = []
-  labeledElements ((key,strings):xs) = processed ++ labeledElements xs
+  len         = getDecimalLen . I.size $ rm
+  alphaHeader = "+-"
+  betaHeader  = "| "
+  gammaHeader = "--"
+  deltaHeader = "  "
+  preprocessed :: [(Int, [String])]
+  -- TODO: Do with accKeys
+  preprocessed =
+    map (\(k, v) -> (k, (showValueStorageWithIdxSub v accKeys (depth + 1))))
+      . I.toList
+      $ rm
+  -- NOTE: headerElements cares whether the element is the last or not
+  headerElements :: [(Int, [String])] -> [String]
+  headerElements []                    = []
+  headerElements ((key, strings) : xs) = processed ++ headerElements xs
    where
-    shown = show key
-    shownLen = length shown
-    header = show depth ++ "=" ++ replicate (len+1-shownLen) '_' ++ show key ++ "-"
-    headerSpace = replicate (length header - 1) ' ' ++ " "
-    processed = if length strings > 0 then (header ++ (head strings)) : map (headerSpace ++) (tail strings) else []
+    bodies = bodyElements key strings
+    -- NOTE: α or γ
+    first  = (if xs == [] then gammaHeader else alphaHeader) ++ head bodies
+    -- NOTE: β or δ
+    rest =
+      map ((if xs == [] then deltaHeader else betaHeader) ++) (tail bodies)
+    processed = first : rest
+    -- NOTE: headerElementsSub cares whether the element is the first or not
+  bodyElements :: Int -> [String] -> [String]
+  bodyElements _   []      = []
+  bodyElements key strings = first : rest
+   where
+    betaBody  = "-"
+    deltaBody = " "
+    shownKey  = show key
+    shownLen  = length shownKey
+    keySpacer = replicate (len - shownLen) '_'
+    first =
+      betaBody
+        ++ show depth
+        ++ "="
+        ++ keySpacer
+        ++ shownKey
+        ++ betaBody
+        ++ head strings
+    bodySpacer = replicate (1 + 1 + 1 + len + 1) ' '
+    rest       = map (bodySpacer ++) . tail $ strings
 
-showValueStorageWithIdxSub (IM im) depth = labeledElements . I.toList $ im
+showValueStorageWithIdxSub (IM im) accKeys depth =
+  labeledElements . I.toList $ im
  where
   len = getDecimalLen . I.size $ im
-  labeledElements [] = []
-  labeledElements ((key,value):xs) = (header ++ show value) : labeledElements xs
+  labeledElements []                  = []
+  labeledElements ((key, value) : xs) = first : labeledElementsSub xs
    where
-    shown = show key
-    shownLen = length shown
-    space = replicate (len+1-shownLen) '_'
-    header = (if xs == [] then "--" else "+-") ++ show depth ++ "=" ++ space ++ shown ++ ": "
+    first =
+      (if xs == [] then "---" else "-+-")
+        ++ show depth
+        ++ "="
+        ++ space
+        ++ shownKey
+        ++ ": "
+        ++ show value
+     where
+      shownKey = show key
+      shownLen = length shownKey
+      space    = replicate (len - shownLen) '_'
+    labeledElementsSub []                  = []
+    labeledElementsSub ((key, value) : xs) = body : labeledElementsSub xs
+     where
+      body =
+        (if xs == [] then " --" else " +-")
+          ++ show depth
+          ++ "="
+          ++ space
+          ++ shownKey
+          ++ ": "
+          ++ show value
+       where
+        shownKey = show key
+        shownLen = length shownKey
+        space    = replicate (len - shownLen) '_'
 
 getDecimalLen num = getDecimalLenSub num 1
-getDecimalLenSub num acc
- | dived > 0 = getDecimalLenSub dived (acc+1)
- | otherwise = acc
- where
-  dived = div num 10
+getDecimalLenSub num acc | dived > 0 = getDecimalLenSub dived (acc + 1)
+                         | otherwise = acc
+  where dived = div num 10
