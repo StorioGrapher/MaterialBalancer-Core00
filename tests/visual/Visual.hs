e4c9800fd048c50e8b99c8abaafc26ed3a592c98
YongJoon Joe <developer@quietjoon.net>
2019-12-31 21:26:15 +0900

    Change test structure

---
+++ tests/visual/Visual.hs
@@ -0,0 +1,11 @@
+main :: IO ()
+main = do
+  putStrLn "Process Visual Test"
+  -- Axis.visual
+  -- Column.visual
+  -- TheTable.visual
+  -- ValueStorage.visual
+  -- DView.visual
+  -- DViewResult.visual
+  -- FView2D.visual
+  -- FViewResult.visual
